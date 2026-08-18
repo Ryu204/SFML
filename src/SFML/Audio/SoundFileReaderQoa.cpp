7308c6634a30dac19eec8690b67aefbbdb48b5eb
Liam Nguyen <baona2@vmogroup.com>
2026-08-18 17:40:17 +0700

    wip

---
+++ src/SFML/Audio/SoundFileReaderQoa.cpp
@@ -0,0 +1,206 @@
+////////////////////////////////////////////////////////////
+//
+// SFML - Simple and Fast Multimedia Library
+// Copyright (C) 2007-2026 Laurent Gomila (laurent@sfml-dev.org)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "SFML/System/Err.hpp"
+#include "SFML/System/InputStream.hpp"
+
+#include <SFML/Audio/SoundFileReaderQoa.hpp>
+
+#include <SFML/System/FileInputStream.hpp>
+
+#include <array>
+#include <optional>
+#include <string_view>
+
+#include <cassert>
+#include <cstdint>
+
+
+namespace
+{
+
+namespace QoaSpecs
+{
+std::uint32_t magicBytes = 0x716F6166; // "qoaf"
+
+constexpr std::size_t maxSampleRate = 0xFFFFFF;
+constexpr std::size_t minSampleRate = 1;
+
+constexpr std::size_t minChannels = 1;
+constexpr std::size_t maxChannels = 8;
+
+constexpr std::size_t fileHeaderSizeByte            = 8;
+constexpr std::size_t frameHeaderSizeByte           = 8;
+constexpr std::size_t lmsStatePerChannelSizeByte    = 16;
+constexpr std::size_t sliceSizeByte                 = 8;
+constexpr std::size_t frameSlicesPerChannelSizeByte = 256 * sliceSizeByte;
+// 1 channel audio, 16 bytes LMS state and 256 slices with 8 bytes each
+constexpr std::size_t frameBodyPerChannelSizeByte = lmsStatePerChannelSizeByte + frameSlicesPerChannelSizeByte;
+} // namespace QoaSpecs
+
+template <typename ReturnType, typename Iter>
+ReturnType readBigEndianUnsignedInt(Iter begin, std::size_t size)
+{
+    assert(size >= 1);
+    ReturnType result = 0;
+    auto       shift  = size * 8;
+    for (std::size_t i = 0; i < size; ++i)
+    {
+        shift -= 8;
+        result += static_cast<ReturnType>(*(begin + static_cast<std::ptrdiff_t>(i))) << shift;
+    }
+    return result;
+}
+
+struct HeaderContent
+{
+    std::uint32_t samplesPerChannel{};
+
+    [[nodiscard]] bool isStreaming() const
+    {
+        return samplesPerChannel == 0;
+    }
+
+    [[nodiscard]] bool isValid() const
+    {
+        return samplesPerChannel >= 0;
+    }
+
+    static std::optional<HeaderContent> readFrom(sf::InputStream& stream)
+    {
+        std::array<std::uint8_t, QoaSpecs::fileHeaderSizeByte> header{};
+
+        if (stream.read(header.data(), header.size()) != header.size())
+            return std::nullopt;
+
+        const auto magicBytes = readBigEndianUnsignedInt<std::uint32_t>(header.begin(), 4);
+        if (magicBytes != QoaSpecs::magicBytes)
+            return std::nullopt;
+
+        const auto samplesPerChannel = readBigEndianUnsignedInt<std::uint32_t>(header.begin(), 4);
+        return HeaderContent{samplesPerChannel};
+    }
+};
+
+struct FrameContent
+{
+    struct Header
+    {
+        std::uint32_t sampleRate;
+        std::uint16_t samplesPerChannel;
+        std::uint16_t frameSizeByte;
+        std::uint8_t  numChannels;
+
+        [[nodiscard]] std::optional<std::string_view> checkError(const HeaderContent& headerContent) const
+        {
+            if (numChannels < QoaSpecs::minChannels || numChannels > QoaSpecs::maxChannels)
+                return "Number of channel in frame is out of bounds";
+            if (sampleRate < QoaSpecs::minSampleRate || sampleRate > QoaSpecs::maxSampleRate)
+                return "Frame sample rate is not in supported range";
+            const auto isFrameSamplesInvalid = headerContent.isStreaming() &&
+                                               samplesPerChannel > headerContent.samplesPerChannel;
+            const auto computedFrameSizeByte = QoaSpecs::frameHeaderSizeByte +
+                                               QoaSpecs::frameBodyPerChannelSizeByte * numChannels;
+
+            const auto isFrameSizeMismatch = frameSizeByte != computedFrameSizeByte;
+            if (isFrameSamplesInvalid || isFrameSizeMismatch)
+                return "Corrupted frame data";
+            return std::nullopt;
+        }
+
+        static std::optional<FrameContent::Header> readFrom(sf::InputStream& stream)
+        {
+            std::array<std::uint8_t, QoaSpecs::frameHeaderSizeByte> frameHeader{};
+            if (stream.read(frameHeader.data(), frameHeader.size()) != frameHeader.size())
+                return std::nullopt;
+
+            const auto numChannels       = frameHeader[0];
+            const auto sampleRate        = readBigEndianUnsignedInt<std::uint32_t>(frameHeader.begin() + 1, 3);
+            const auto samplesPerChannel = readBigEndianUnsignedInt<std::uint16_t>(frameHeader.begin() + 4, 2);
+            const auto frameSizeByte     = readBigEndianUnsignedInt<std::uint16_t>(frameHeader.begin() + 6, 2);
+            return Header{sampleRate, samplesPerChannel, frameSizeByte, numChannels};
+        }
+    };
+
+    Header header;
+};
+} // namespace
+
+namespace sf::priv
+{
+////////////////////////////////////////////////////////////
+bool SoundFileReaderQoa::check(InputStream& stream)
+{
+    const auto headerContent = HeaderContent::readFrom(stream);
+    if (!headerContent)
+        return false;
+    if (!headerContent->isValid())
+        return false;
+    const auto frameHeader = FrameContent::Header::readFrom(stream);
+    if (!frameHeader)
+        return false;
+    const auto frameHeaderError = frameHeader->checkError(*headerContent);
+    return !frameHeaderError.has_value();
+}
+
+
+////////////////////////////////////////////////////////////
+std::optional<SoundFileReader::Info> SoundFileReaderQoa::open(InputStream& stream)
+{
+    const auto headerContent = HeaderContent::readFrom(stream);
+    if (!headerContent || !headerContent->isValid())
+    {
+        err() << "Header of QOA file was incorrectly verified or recently changed" << std::endl;
+        return std::nullopt;
+    }
+    const auto firstFrameHeader = FrameContent::Header::readFrom(stream);
+    if (!firstFrameHeader)
+    {
+        err() << "Failed to read first frame header in QOA file" << std::endl;
+        return std::nullopt;
+    }
+    if (const auto error = firstFrameHeader->checkError(*headerContent); error)
+    {
+        err() << "Invalid QOA file frame header: " << *error << std::endl;
+    }
+    m_channelCount =
+}
+
+
+////////////////////////////////////////////////////////////
+void SoundFileReaderQoa::seek(std::uint64_t sampleOffset)
+{
+}
+
+
+////////////////////////////////////////////////////////////
+std::uint64_t SoundFileReaderQoa::read(std::int16_t* samples, std::uint64_t maxCount)
+{
+    return 0;
+}
+
+} // namespace sf::priv
