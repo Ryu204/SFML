60fac9888865cedbb54d0c820a743101ed70bcac
Ryu204 <nguyenanhbao2356@gmail.com>
2026-09-18 23:14:49 +0700

    Add support for QOA sound format

--- src/SFML/Audio/SoundFileFactory.cpp
+++ src/SFML/Audio/SoundFileFactory.cpp
@@ -29,6 +29,7 @@
 #include <SFML/Audio/SoundFileReaderFlac.hpp>
 #include <SFML/Audio/SoundFileReaderMp3.hpp>
 #include <SFML/Audio/SoundFileReaderOgg.hpp>
+#include <SFML/Audio/SoundFileReaderQoa.hpp>
 #include <SFML/Audio/SoundFileReaderWav.hpp>
 #include <SFML/Audio/SoundFileWriterFlac.hpp>
 #include <SFML/Audio/SoundFileWriterOgg.hpp>
@@ -144,7 +145,8 @@ SoundFileFactory::ReaderFactoryMap& SoundFileFactory::getReaderFactoryMap()
     static ReaderFactoryMap result{{&priv::createReader<priv::SoundFileReaderFlac>, &priv::SoundFileReaderFlac::check},
                                    {&priv::createReader<priv::SoundFileReaderMp3>, &priv::SoundFileReaderMp3::check},
                                    {&priv::createReader<priv::SoundFileReaderOgg>, &priv::SoundFileReaderOgg::check},
-                                   {&priv::createReader<priv::SoundFileReaderWav>, &priv::SoundFileReaderWav::check}};
+                                   {&priv::createReader<priv::SoundFileReaderWav>, &priv::SoundFileReaderWav::check},
+                                   {&priv::createReader<priv::SoundFileReaderQoa>, &priv::SoundFileReaderQoa::check}};
 
     return result;
 }
